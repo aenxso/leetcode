b809bf6f72fa63218f76afd8f2a90cb31323cb4e
aenxso <32026487+aenxso@users.noreply.github.com>
2021-08-29 06:27:14 -0700

    Update construct-the-rectangle.cpp
    add comments

--- construct-the-rectangle/construct-the-rectangle.cpp
+++ construct-the-rectangle/construct-the-rectangle.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        int width = sqrt(area);
-        while(area % width) {
+        int width = sqrt(area); //width will always be <= to sqrt of area
+        while(area % width) { //while the division is not perfect, reduce the square root (width) value
             width--;
         }
         
