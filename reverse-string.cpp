06dfca96e8d47e2c3ca214df022141010b25fec7
aenxso <32026487+aenxso@users.noreply.github.com>
2021-10-18 06:06:54 -0700

    Create reverse-string.cpp
    add Leetcode question #344 - Reverse String

---
+++ reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+    // two pointer method - continue swapping from edges inward
+        int l = 0, r = s.size() - 1;
+        while(l <= r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+};
