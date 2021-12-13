e4b55290f2bf3a6374b412eb8d37f3b36dba716b
aenxso <32026487+aenxso@users.noreply.github.com>
2021-12-13 03:00:48 -0800

    Update rotate-array.cpp
    add comment

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -5,7 +5,7 @@ public:
         //reverse 0 to n-k
         //reverse entire thing
         int len = nums.size();
-        int n = k % len;
+        int n = k % len; // k may be bigger than nums.size(), but it's still acceptable to rotate x number of times
         reverse(nums.end() - n, nums.end());
         reverse(nums.begin(), nums.end() - n);
         reverse(nums.begin(), nums.end());
