c463f9c2188abfb3e0c31ee072dc7b8cab40cc29
aenxso <32026487+aenxso@users.noreply.github.com>
2021-12-21 06:01:35 -0800

    Time: 4 ms (41.22%), Space: 5.8 MB (79.35%) - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        while (n > 0) { // iterate until all bits are traversed
+            if ((n & 1) > 0) { // check the parity of first bit from right
+                cnt++;
+            }
+            n >>= 1; // n = n/2, shift one bit to right
+        }
+        return cnt;
+    }
+};
