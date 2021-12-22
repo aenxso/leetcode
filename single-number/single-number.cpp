37377c2d278ff2f66946b2a2861e5082839d58f1
aenxso <32026487+aenxso@users.noreply.github.com>
2021-12-21 20:41:03 -0800

    Time: 24 ms (35.27%), Space: 16.9 MB (83.67%) - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            result = result ^ nums[i];
+        for(int i : nums) {
+            result = result ^ i; // XOR order doesn't matter, so XORing al elements together causes duplicates 
+                                 // to cancel each other out (become 0), and single number is left
+            
         }
         return result;
     }
