bb3dad69acfdff4805280f1bda32060fe344ac83
aenxso <32026487+aenxso@users.noreply.github.com>
2021-12-21 21:23:57 -0800

    Time: 0 ms (100.00%), Space: 7.6 MB (70.80%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -3,12 +3,12 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
         if(nums.size() == 2) return max(nums[0], nums[1]);
-        int total = 0, prevTotal = 0;
+        int total = 0, prevTotal = 0; // prevTotal = i-2th house
         
         for(int i = 0; i < nums.size(); i++) {
-            int tmpTotal = total;
+            int tmpTotal = total; // tmpTotal stores the i-1th house
             total = max(total, nums[i] + prevTotal); //either don't rob this one (total is bigger), or rob (this house + i-2th) house
-            prevTotal = tmpTotal;
+            prevTotal = tmpTotal; // i-2th becomes i-1th 
         }
         return total;
     }
