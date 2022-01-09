4cc2a8185a1b47f441f6ce1067472ffc1426fd62
aenxso <32026487+aenxso@users.noreply.github.com>
2022-01-09 06:03:21 -0800

    Time: 0 ms (100.00%), Space: 7.3 MB (85.82%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n = nums.size(), sum = 0;
-        dfs(nums, n - 1, sum);
-        return sum;
-    }
-    
-private:
-    int dfs(vector<int>& nums, int i, int& sum) {
-        // base case
-        if(i < 2) return 0;
-        
-        int res = 0;
-        if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
-            res = 1 + dfs(nums, i - 1, sum);
-            sum += res;
-        } else {
-            dfs(nums, i - 1, sum);
+        int dp = 0, totalSum = 0;
+        for(int i = 2; i < nums.size(); i++) {
+            if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                dp++;
+                totalSum += dp;
+            } else {
+                dp = 0;
+            }
         }
-        return res;
+        return totalSum;
     }
+
 };
