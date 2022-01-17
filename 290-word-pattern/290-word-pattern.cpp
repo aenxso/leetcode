03d4ef082dd51014eee4c533852b09ab4a01f20f
aenxso <32026487+aenxso@users.noreply.github.com>
2022-01-16 21:54:28 -0800

    Time: 0 ms (100.00%), Space: 6.4 MB (50.50%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        // map a character in the pattern to a word in the string, and map the string back onto the same character
+        // loop through and make sure there is only one to one mapping for each char/word (bijection),
+        // and that each char is mapped to at least one word
+        
+        int n = pattern.size(), i = 0;
+        vector<int> pat(26, 0);
+        unordered_map<string, int> map;
+        istringstream ss (s);
+        string word;
+        
+        for(string word; ss >> word; i++) {
+            if(i == n || pat[pattern[i] - 'a'] != map[word]) {
+                return false;
+            }
+            pat[pattern[i] - 'a'] = i + 1; // 1-based indexing since map assigns 0 to keys not found (cr. @lllllll2)
+            map[word] = i + 1;
+        }
+        
+        return i == n;
+    }
+};
