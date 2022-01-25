ec9bd09500e9e9c72f0cf2ec3adf178a88f6aa90
aenxso <32026487+aenxso@users.noreply.github.com>
2022-01-25 03:02:51 -0800

    Time: 8 ms (73.64%), Space: 10.7 MB (96.77%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        if(nums.size() == 0) return ans;
-        vector<int> comb;
+        if(candidates.size() == 0) return ans;
+        vector<int> comb; // vector to store possible combinations
         
-        backtrack(nums, ans, comb, target, 0);
+        comboFinder(candidates, ans, comb, target, 0);
         
         return ans;
     }
     
 private:
-    void backtrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& comb, int target, int begin) {
+    void comboFinder(vector<int>& nums, vector<vector<int>>& ans, vector<int>& comb, int target, int begin) {
         // subtract current value from target each time so we know how much more we need to add to reach the target
         // when target reaches a perfect 0, we know we have a combination
         // if target becomes less than 0, no solution with current combination
@@ -23,7 +23,7 @@ private:
         
         for(int i = begin; i < nums.size(); i++) {
             comb.push_back(nums[i]);
-            backtrack(nums, ans, comb, target - nums[i], i);
+            comboFinder(nums, ans, comb, target - nums[i], i);
             comb.pop_back();
         }
     }
