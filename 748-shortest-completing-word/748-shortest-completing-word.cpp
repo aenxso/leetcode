fcdf8f65f590633cd4486ea4ea30bfca9522dd7d
aenxso <32026487+aenxso@users.noreply.github.com>
2022-01-25 20:21:36 -0800

    Time: 20 ms (55.35%), Space: 16 MB (50.94%) - LeetHub

--- 748-shortest-completing-word/748-shortest-completing-word.cpp
+++ 748-shortest-completing-word/748-shortest-completing-word.cpp
@@ -4,23 +4,23 @@ public:
         vector<int> freq(26, 0);
         string shortest = "";
         
-        for(int i = 0; i < licensePlate.size(); i++) {
+        for(int i = 0; i < licensePlate.size(); i++) { // add frequency of all letters to a vector
             if(isalpha(licensePlate[i])) {
                 freq[tolower(licensePlate[i]) - 'a']++;
             }
         }
         
         for(int i = 0; i < words.size(); i++) {
-            vector<int> tmp = freq;
+            vector<int> tmp = freq;                    // copy freq vector to use for each word in words
             for(int j = 0; j < words[i].size(); j++) {
                 tmp[tolower(words[i][j]) - 'a']--;
             }
             bool flag = true;
-            for(int j = 0; j < tmp.size(); j++) {
+            for(int j = 0; j < tmp.size(); j++) {      // frequency doesn't match, not a suitable word
                 if(tmp[j] > 0) flag = false;
             }
             
-            if(flag && (shortest == "" || words[i].length() < shortest.length())) {
+            if(flag && (shortest == "" || words[i].length() < shortest.length())) { // check for new shortest word
                 shortest =  words[i];
             }
         }
