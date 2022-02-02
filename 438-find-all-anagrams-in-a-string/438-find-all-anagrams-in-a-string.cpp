3c3ee48c790fa4a4113f2c699d0b7185f9245628
aenxso <32026487+aenxso@users.noreply.github.com>
2022-02-02 01:30:43 -0800

    Time: 21 ms (52.75%), Space: 8.7 MB (76.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        // sliding window problem
-        // create vectors to store frequency of letter appearance in both strings, then compare them in a loop 
-        //    while changing window size
-        // window size can only be the size of p
+        // create window of size p
+        // two pointers to maintain window size
         
-        int n = s.size(), windowSz = p.size();
-        if(n < windowSz) return {}; // can't compare a string that is smaller than the necessary window size
-        vector<int> ans, sFreq(26, 0), pFreq(26, 0);
-        int right = 0, left = 0;
+        if(s.size() < p.size()) return {};
         
-        // for first itr ONLY, fill frequency arrays of both up to the window size
-        while(right < windowSz) {
-            sFreq[s[right] - 'a'] += 1;
-            pFreq[p[right] - 'a'] += 1;
-            right++;
+        vector<int> ans, freqS(26, 0), freqP(26, 0);
+        int l = 0, r = 0;
+        
+        // for ONLY the first window of size p
+        int window = p.size();
+        while(r < window) {
+            
+            freqS[s[r] - 'a'] += 1;
+            freqP[p[r] - 'a'] += 1;
+            r++;
         }
         
-        right--;
-        while(right < n) {
-            if(sFreq == pFreq) {            // if frequencies are a match, add to answer
-                ans.push_back(left);
+        r--;
+        while(r < s.size()) {
+            
+            if(freqS == freqP) ans.push_back(l);
+            r++;
+            if(r != s.size()) {
+                freqS[s[l] - 'a'] -= 1;
+                freqS[s[r] - 'a'] += 1;
             }
-            right++;                        // increase window opening by one to the right
-            if(right != n) {
-                sFreq[s[right] - 'a'] += 1; // adjust frequency of newly added letter by increasing
-                sFreq[s[left] - 'a'] -= 1;  // adjust frequency of letter that is to be removed by decreasing
-            }
-            left++;                         // increase window closing by one to the right (cutting off last letter)
+            l++;
         }
         return ans;
     }
