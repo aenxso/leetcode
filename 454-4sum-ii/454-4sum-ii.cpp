5f19f452e942e35e7990b361eb69c8711e9cbe34
aenxso <32026487+aenxso@users.noreply.github.com>
2022-02-03 02:41:02 -0800

    Time: 380 ms (33.34%), Space: 24.5 MB (36.00%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        int ans = 0, n = nums1.size();
-        unordered_map<int, int> sums;
+        int ans = 0, n = nums1.size(); // all arrs are same size, so can use any for n
+        unordered_map<int, int> sums; // map to store the first calculated value, to compare to second calculated value
+        
+        // get a value from first two arrays
+        // increment value at location nums1[i] + nums2[j]
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 sums[nums1[i] + nums2[j]]++;
             }
         }
         
+        // check if matching pair exists in next two arrays such that first sum + second sum == 0
+        // if it exists, update ans
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                ans += sums[-(nums3[i] + nums4[j])];
+                ans += sums[-(nums3[i] + nums4[j])]; // += value because duplicate tuples are also valid - count++ is not enough
             }
         }
         return ans;
