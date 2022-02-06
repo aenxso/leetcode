184c8b5257fa9dacdf1088a5dc0b26d9676ffc69
aenxso <32026487+aenxso@users.noreply.github.com>
2022-02-06 04:59:07 -0800

    Time: 15 ms (12.45%), Space: 11 MB (37.19%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int count = 0, num = nums[0]; // count keeps track of how many times we've seen num
-        
-        for(int i = 0; i < nums.size() - 1; i++) {     
-            if(num == nums[i + 1]) {
-                count++;
-                if(count >= 2) {
-                    int j = i; // create separate variable for while loop, otherwise main loop itrs get messed up
-                    while(num == nums[j] && j < nums.size()) {
-                        nums.erase(nums.begin() + j);
-                        j++;
-                    }
-                    i--;
-                }
-            } else {
-                count = 0; // reset count if num is different
-                num = nums[i + 1];
+        int i = 0;
+        for(int num : nums) {
+            if(i < 2 || num > nums[i - 2]) {
+                nums[i] = num;
+                i++;
             }
         }
-        return nums.size();
+        return i;
     }
 };
