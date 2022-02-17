5381a115def4b97222ce4a8a6f5f6eea968d0632
aenxso <32026487+aenxso@users.noreply.github.com>
2022-02-17 04:49:52 -0800

    Time: 0 ms (100.00%), Space: 10.8 MB (72.35%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -3,25 +3,26 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> combo; // vector to store possible combinations of numbers
-        backtrack(candidates, ans, combo, target, 0);
+        int sum = 0;
+        backtrack(candidates, ans, combo, target, 0, sum);
         
         return ans;
     }
     
 private:
-    void backtrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& combo, int target, int begin) {
+    void backtrack(vector<int>& nums, vector<vector<int>>& ans, vector<int>& combo, int target, int begin, int sum) {
         // subtract current value from target each time so we know how much more we need to add to reach the target
         // when target reaches a perfect 0, we know we have a combination
         // if target becomes less than 0, no solution with current combination - return
         
-        if(target < 0) return;
-        if(target == 0) {
+        if(sum > target) return;
+        if(sum == target) {
             ans.push_back(combo);
         }
         
         for(int i = begin; i < nums.size(); i++) {
             combo.push_back(nums[i]);
-            backtrack(nums, ans, combo, target - nums[i], i);
+            backtrack(nums, ans, combo, target, i, sum + nums[i]);
             combo.pop_back();
         }
     }
