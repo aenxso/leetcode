3add072b70ad23f9530c7f3bbb4a87d4db75a44f
aenxso <32026487+aenxso@users.noreply.github.com>
2022-02-18 05:13:54 -0800

    Time: 5 ms (58.25%), Space: 7.1 MB (52.75%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
+        // go through every digit, and if the preceding value is higher, delete it from the string
+        // use stack to keep track of the digits we are keeping
+        
         if(k == num.length()) return "0";
         
         string ans = "";
@@ -14,11 +17,14 @@ public:
             stack.push_back(num[i]);
         }
         
+        // delete more values if k still isn't at 0
         while(k > 0) {
             k--;
             stack.pop_back();
         }
         
+        // add stack chars (digits) to string
+        // skip any preceding 0's
         for(char c : stack) {
             if(c == '0' && ans.empty()) continue;
             ans += c;
