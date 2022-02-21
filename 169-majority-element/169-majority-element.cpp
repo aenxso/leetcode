d7cc63675b2e2c1b800fcd42f4f771ac7735c6b5
aenxso <32026487+aenxso@users.noreply.github.com>
2022-02-21 05:11:19 -0800

    Time: 28 ms (47.28%), Space: 19.7 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // Moore's algorithm
+        // keep a count and cancel out every occurrence of an element if the next element is different
+        // the only element that will still be left by the end is the majority element
+        
         int majorityIndex = 0;
         for(int count = 1, i = 1; i < nums.size(); i++) {
             nums[majorityIndex] == nums[i] ? count++ : count--;
