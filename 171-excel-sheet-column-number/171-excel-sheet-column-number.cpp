57ec5ff76df8d0801413560fd6cf2ec1165e6e53
aenxso <32026487+aenxso@users.noreply.github.com>
2022-02-24 04:24:21 -0800

    Time: 0 ms (100.00%), Space: 5.9 MB (89.47%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-        // multiply by 26 for every letter in string (base 26 basically, aside from first letter)
-        // add on numerical value of each individual letter for every itr
-        
         int ans = 0;
         
         for(char c : columnTitle) {
-            int addValue = c - 'A' + 1;
-            ans = ans * 26 + addValue;
+            int letterVal = c - 'A' + 1;
+            ans = ans * 26 + letterVal;
         }
-        
         return ans;
     }
 };
