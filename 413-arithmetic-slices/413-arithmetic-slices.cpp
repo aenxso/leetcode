8d6033e9d8fa780b82d2757c2b4cfeb9b20d290b
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-03 03:58:46 -0800

    Time: 0 ms (100.00%), Space: 7.4 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        // solution using a dp array
-        // time and space are O(n)
-        // can be optimized to get rid of array and just use an int value
+        // solution using an int value to keep track of whether or not there was a slice
+        // since we are looking at 3-element windows, we have to keep moving up our window and
+        //   using a dp int keeps track of whether the elements before were also a sequence
+        //   as we can keep adding onto it. This ensures we can check every slice without
+        //   worrying about restting the start and end points when a sequence ends.
+        // reset dp to 0 when the sequence breaks 
+        // time: O(n), space: O(1)
         
         if(nums.size() < 3) return 0;
-        int ans = 0;
-        vector<int> dp(nums.size());
+        int ans = 0, dp = 0;
         
-        for(int i = 2; i < nums.size(); i++) {
+        // start at because anything less than 2 won't work, and we need to go back that many spaces
+        for(int i = 2; i < nums.size(); i++) { 
             if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
-                dp[i] = 1 + dp[i - 1];
-                ans += dp[i];
+                dp++;
+                ans += dp;
+            } else {
+                dp = 0; // reset dp to 0 if the sequence is disrupted
             }
         }
         return ans;
