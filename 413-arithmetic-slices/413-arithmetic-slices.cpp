cfa78371b647e15d6364c353a990bd66fc6f311d
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-03 03:49:33 -0800

    Time: 0 ms (100.00%), Space: 7.4 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int dp = 0, totalArrs = 0;
-        for(int i = 2; i < nums.size(); i++) { // start at 2 because we need at least elements - anything less than 2 won't work
+        if(nums.size() < 3) return 0;
+        int ans = 0;
+        vector<int> dp(nums.size());
+        
+        for(int i = 2; i < nums.size(); i++) {
             if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
-                dp++;
-                totalArrs += dp; // if the differences are the same we will have dp number additional combos of arith slices
-            } else {
-                dp = 0; // reset dp if differences are different
+                dp[i] = 1 + dp[i - 1];
+                ans += dp[i];
             }
         }
-        return totalArrs;
+        return ans;
     }
 };
