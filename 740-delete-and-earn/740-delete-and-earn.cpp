68f7fbf9ed265841a65619c6450e68d68b8b005b
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-05 05:35:12 -0800

    Time: 16 ms (35.15%), Space: 11.6 MB (28.77%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
+        // similar to house robber
+        // use a frequency vector to store occurrences of numbers in nums
+        // if we pick one number, i, we can safely pick all occurrence of that number and
+        //  add the points
+        // now. we cannot pick i-1 but we can pick i-2 which we call prevMax
+        
         int ans = 0, prevMax = 0;
         vector<int> freq(10001, 0);
         
