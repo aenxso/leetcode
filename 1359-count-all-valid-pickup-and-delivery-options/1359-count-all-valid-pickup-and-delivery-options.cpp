e2d9273b6d737f33fc8001508b4e0634b7744727
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-06 04:28:38 -0800

    Time: 0 ms (100.00%), Space: 5.9 MB (53.14%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int countOrders(int n) {
+        /*
+            from lee215:
+            Assume we have already n - 1 pairs, now we need to insert the nth pair.
+            To insert the first element, there are n * 2 - 1 chioces of position。
+            To insert the second element, there are n * 2 chioces of position。
+            So there are (n * 2 - 1) * n * 2 permutations.
+            Considering that delivery(i) is always after of pickup(i), we need to divide 2.
+            So it's (n * 2 - 1) * n        
+        */
+        
         long mod = 1e9 + 7, ans = 1;
         
         for(int i = 1; i <= n; i++) {
