6209bbb944442968f86f6132284feb3eeefafd48
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-07 03:59:30 -0800

    Time: 3 ms (97.67%), Space: 14.8 MB (45.55%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,17 +11,23 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        // recursive method
+        ListNode dummy = NULL;
+        ListNode* sorter = &dummy;
         
-        if(!l1) return l2;
-        if(!l2) return l1;
         
-        if(l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
+        while(l1 && l2) {
+            if(l1->val < l2->val) {
+                sorter->next = l1;
+                l1 = l1->next;
+            } else {
+                sorter->next = l2;
+                l2 = l2->next;
+            }
+            sorter = sorter->next; // remember to increment sorter otherwise we'll just keep overwriting the same value
         }
+       
+       sorter->next = l1 ? l1 : l2;
+        
+        return dummy.next;
     }
 };
