a216782656fee7e1c98ff73e383839677092054f
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-12 04:12:31 -0800

    Time: 12 ms (58.04%), Space: 11.3 MB (60.48%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,19 +17,29 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        // create a hashmap which maps the original node to a newly created node
+        // use one pass to create this map, note that new list has no connections yet (next/random)
+        // on second pass, create the next and random links by utilizing the constant access 
+        //   to a certain node in both lists. eg. map[clone]->next = map[original->next]
+        //   rmbr map[original->next] is still pointing to the cloned list, so the deep copy properties still hold
+        
         if(!head) return NULL;
-        Node* itr = head;
+        Node* itr = head; // create a node to iterate through list as needed
         unordered_map<Node*, Node*> map;
+        
+        // 1st pass - create a copy of each node which the original's value
         while(itr) {
             map[itr] = new Node(itr->val);
             itr = itr->next;
         }
-        itr = head;
+        itr = head; // reset itr back to the beginning for 2nd pass
+        
+        // 2ns pass - create the next and random links for the cloned list
         while(itr) {
             map[itr]->next = map[itr->next];
             map[itr]->random = map[itr->random];
             itr = itr->next;
         }
-        return map[head];
+        return map[head]; // return head of cloned list, as stored in map
     }
 };
