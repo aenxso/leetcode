91591ec6cb836a3ce0cc9c26d2eb51e0bfbe036e
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-21 01:18:17 -0700

    Time: 4 ms (72.57%), Space: 6.5 MB (96.62%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -6,19 +6,19 @@ public:
         //  of another letter, we increase the partition to avoid splitting the newly found letter into two partitions
         //  since the requirement is having any letter appear at most once in a partition
         
-        vector<int> endings(26, 0), ans;
+        vector<int> endings(26, 0), ans; // size 26 for all letters in alphabet
         int start = 0, last = 0, n = s.length();
         
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {     // add last element for all letters present in s
             endings[s[i] - 'a'] = i; 
         }
         
         for(int i = 0; i < n; i++) {
-            last = max(last, endings[s[i] - 'a']);
-            
-            if(i == last) {
+            last = max(last, endings[s[i] - 'a']); // finding the maximum end index for all letters currently encountered
+              
+            if(i == last) {      // if we're at the last max found index, create partition here
                 ans.push_back(i - start + 1);
-                start = i + 1;
+                start = i + 1;   // adjust start for next partition
             }
         }
         return ans;
