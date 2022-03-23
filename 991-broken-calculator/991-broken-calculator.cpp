2dad9103c6642c1c0d3998af0ab647a94343dbd1
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-22 21:17:24 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
+        // works backwards - treat startValue as target, and target as startValue
+        // decrement target until it is LESS THAN startValue
+        // if target is even: /= 2, if target is odd += 1
+        // once it's less than the only operation that makes sense is adding 1
+        // so answer will be ans plus the difference between startValue and target as we have to make up
+        //  that difference by +1'ing, and each +1 counts as one operation that counts towards the answer total
+        
         if(startValue == target) return 0;
-        if(startValue > target) return startValue - target;
+        if(startValue > target) return startValue - target; // return diff as only operation is -1
         int ans = 0;
         
         while(startValue < target) {
-            if(target % 2 == 0) {
+            if(target % 2 == 0) { // if even then divide by 2
                 target /= 2;
                 ans++;
-            } else {
+            } else {              // if odd, add one to make it even
                 target += 1;
                 ans++;
             }
