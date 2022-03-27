cd444a014b8286e2ad71b9b8f5c3772bb896ed38
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-27 14:51:19 -0700

    Time: 38 ms (5.43%), Space: 10.6 MB (42.40%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        // create a set of pairs - first element in pair is the count of ones in the row,
+        //    second element is index
+        // pairs automatically get sorted in ascending order based on first element (count of ones in this case)
+        // using stl function count to count ones in rows
+        
         vector<int> ans;
         set<pair<int, int>> s;
         
         for(int i = 0; i < mat.size(); i++) {
-            int ones = count(mat[i].begin(), mat[i].end(), 1);
-            s.insert({ones, i});
+            int ones = count(mat[i].begin(), mat[i].end(), 1); // stl function
+            s.insert({ones, i}); // insert new pair into set
         }
         
-        for(auto x : s) {
+        for(auto x : s) { // add elements to answer k number of times
             if(k == 0) break;
             ans.push_back(x.second);
             k--;
