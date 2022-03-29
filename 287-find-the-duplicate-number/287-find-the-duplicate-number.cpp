699d8e8e36a07ad67ae9d3e1f97b6a37e82fc73e
aenxso <32026487+aenxso@users.noreply.github.com>
2022-03-29 03:05:46 -0700

    Time: 178 ms (44.41%), Space: 61.2 MB (91.55%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        // same logic as finding a loop in a linked list
+        // instead of going to the next element in line, pointer uses values as indexes
+        
         if(nums.size() < 2) return -1;
+        
         int slow = nums[0], fast = nums[nums[0]];
         while(slow != fast) {
             slow = nums[slow];
