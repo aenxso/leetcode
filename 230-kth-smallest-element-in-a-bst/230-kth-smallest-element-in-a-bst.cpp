96415e05a35dba5b5d626283dc7244f24f478870
aenxso <32026487+aenxso@users.noreply.github.com>
2022-04-18 02:01:24 -0700

    Time: 14 ms (93.29%), Space: 24.2 MB (60.91%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -12,6 +12,11 @@
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
+        // iterative approach
+        // create a stack of type TreeNode* and store the nodes from root->leftmost
+        // pop stack k times by the time we reach end of BST and use newly created node
+        //  to traverse to check the right child (completes the inorder traversal, otherwise values would be out of order)
+        
         stack<TreeNode*> stk;
         TreeNode* node = root;
         
@@ -21,9 +26,9 @@ public:
                 node = node->left; // go all the way to the left most node (smallest value)
             }
             node = stk.top();
-            if(--k == 0) return node->val;
+            if(--k == 0) return node->val; // only traverse k times
             stk.pop();
-            node = node->right;
+            node = node->right; // check the right child to keep inorder traversal
         }
         return 0;
     }
