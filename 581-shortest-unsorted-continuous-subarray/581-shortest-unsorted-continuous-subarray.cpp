0b9fd5a0defdfc5272eaa06732d6ab9c768d33ef
aenxso <32026487+aenxso@users.noreply.github.com>
2022-05-02 22:36:41 -0700

    Time: 32 ms (83.37%), Space: 26.6 MB (68.90%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
+        // use two pointers - find the beginning and end of the unsorted subarray
+        
         int n = nums.size(), i = 0, j = n - 1, ans = 0;
         
         for(; i < n - 1; i++) {
@@ -18,7 +20,7 @@ public:
         if(i == n - 1) return 0; // array is already sorted
        
         
-        // if there are duplicate numbers in the unsorted subarray, we need to include them
+        // if there are duplicate numbers in the unsorted subarray, we need to include them ie. [1,3,2,2,2]
         
         int minVal = INT_MAX, maxVal = INT_MIN;
         for(int m = i; m <= j; m++) {
@@ -26,6 +28,7 @@ public:
             maxVal = max(maxVal, nums[m]);
         }
         
+        // extend the temporary window
         while(i > 0 && nums[i - 1] > minVal) i--;
         while(j < n - 1 && nums[j + 1] < maxVal) j++;
         
