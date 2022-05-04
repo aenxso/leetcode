03b62c9622adb4cb093f4f1ac3d3266ca5bcd173
aenxso <32026487+aenxso@users.noreply.github.com>
2022-05-04 01:44:54 -0700

    Time: 222 ms (35.68%), Space: 72.8 MB (26.85%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
+        // one pass
+        // time: O(N) space: O(N)
+        // use a map to store the nums encountered, if a complement of the current number is found
+        //   increase the answer and 'take it out' of the array by reducing the frequency in the map
+        
         unordered_map<int, int> freq;
         int ans = 0;
         
