723bfabe86d4012bb3d0e61489001bf962ee463a
aenxso <32026487+aenxso@users.noreply.github.com>
2022-05-16 16:51:45 -0700

    Time: 355 ms (16.32%), Space: 74.9 MB (12.27%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -7,18 +7,19 @@ public:
         
         queue<pair<int, int>> path;
         path.push({0, 0});
-        grid[0][0] = 1;
+        grid[0][0] = 1; // change coordinates of grid as we go along by increasing by 1 -- this changes the values away
+                        //  from 0 so no retracing occurs, and gives us the length of the path
         
         while(!path.empty()) {
             auto cur = path.front();
             int x = cur.first, y = cur.second;
-            if(x == m - 1 && y == n - 1) return grid[x][y]; // reached the last coordinate
+            if(x == m - 1 && y == n - 1) return grid[x][y]; // reached the last coordinate, return the last updated value of grid
             
-            for(auto dir : dirs) {
+            for(auto dir : dirs) {                          // explore all 8 directions to find 0
                 int newX = x + dir[0], newY = y + dir[1];
-                if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == 0) {
-                    path.push({newX, newY});
-                    grid[newX][newY] = grid[x][y] + 1;
+                if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == 0) { // chack if in bounds
+                    path.push({newX, newY});           // add new coordinate to path to explore
+                    grid[newX][newY] = grid[x][y] + 1; // update the path length by changing grid value
                 }
             }
             path.pop();
