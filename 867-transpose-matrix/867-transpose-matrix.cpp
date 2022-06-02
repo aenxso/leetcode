3e68e97c4dcf931cf4021e1aa381ca3c8e73fc55
aenxso <32026487+aenxso@users.noreply.github.com>
2022-06-02 15:55:55 -0700

    Time: 16 ms (54.27%), Space: 10.6 MB (42.65%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         // time: O(n*m), space: O(n*m)
+        // make sure to flip n and m when creating the ans vector (mxn instead of nxm)
         
         int n = matrix.size(), m = matrix[0].size();
         vector<vector<int>> ans(m, vector<int>(n));
