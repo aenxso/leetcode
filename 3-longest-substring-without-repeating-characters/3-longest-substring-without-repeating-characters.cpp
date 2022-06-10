11b4275ea1a6f3523957a9a2a545d8d276df6712
aenxso <32026487+aenxso@users.noreply.github.com>
2022-06-10 16:59:30 -0700

    Time: 47 ms (25.91%), Space: 10.8 MB (30.97%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         if(s.length() == 0) return 0;
+        unordered_set<char> seen;
         
-        unordered_set<char> chars; // set to log all chars currently in substring
         int l = 0, r = 0, n = s.length(), ans = 0;
         while(l < n && r < n) {
-            if(chars.find(s[r]) == chars.end()) { // if the current char is not yet in set
-                chars.insert(s[r++]);
+            if(seen.find(s[r]) == seen.end()) {
+                seen.insert(s[r++]);
                 ans = max(ans, (r - l));
             } else {
-                chars.erase(s[l++]); // duplicate found - erase from set and make window smaller
-                                     // if duplicate is in the middle of the substring, this step will repeat 
-                                     // until the duplicate is reached and deleted - making window smaller everytime
+                seen.erase(s[l++]);
             }
         }
         return ans;
