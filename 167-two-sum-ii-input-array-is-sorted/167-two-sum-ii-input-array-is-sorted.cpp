aac85997e05444dec28aca379e021edb3ef2ca19
aenxso <32026487+aenxso@users.noreply.github.com>
2022-06-10 17:08:10 -0700

    Time: 24 ms (28.02%), Space: 15.6 MB (75.12%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        // two pointer method. Adjust pointer position based on calculated sum vs. target
+        // use two pointers
+        // since input array is sorted in increasing order, either increase left pointer
+        // or decrease right pointer to adjust sum to reach the target
+        
         vector<int> ans;
         int l = 0, r = numbers.size() - 1;
         while(l < r) {
-            if((numbers[l] + numbers[r]) == target) {
-                ans.push_back(l + 1);   // add one because indices are 1-indexed
+            if(numbers[l] + numbers[r] == target) {
+                ans.push_back(l + 1); // add 1 because array is 1-indexed
                 ans.push_back(r + 1);
                 return ans;
             } else if (numbers[l] + numbers[r] > target) {
                 r--;
-            } else {
+            } else if(numbers[l] + numbers[r] < target) {
                 l++;
             }
         }
