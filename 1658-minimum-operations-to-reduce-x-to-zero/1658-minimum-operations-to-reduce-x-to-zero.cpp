e5a64103e3b69b5291a629f659e5a3338cef5eb3
aenxso <32026487+aenxso@users.noreply.github.com>
2022-06-13 03:00:31 -0700

    Time: 205 ms (83.85%), Space: 98.4 MB (97.86%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
+        // find a window of the longest length which has a sum equal to (sum - x)
+        // when such a window is found, all the elements outside this window will sum 
+        //   to x and the number of operations will be equal to their count
         
-        int n = nums.size(), s = 0, ans = 1000000;
+        int n = nums.size(), sum = 0, ans = 1000000;
         
-        for(int i = 0; i < n ; i++) s += nums[i];
+        for(int i = 0; i < n; i++) {
+            sum += nums[i]; // create sum of all numbers in array
+        }
         
-        if(s < x) return -1;
+        if(sum < x) return -1; // if all element don't even add up to x, then no solution is possible
         
-        int st = 0, end = 0, windowSum = 0;
+        int l = 0, r = 0, windowSum = 0;
         
-        while(end < n){            
-            windowSum += nums[end];            
-            while(windowSum > (s - x)){
-                windowSum -= nums[st]; // reducing window size
-                st++;
+        while(r < n){            
+            windowSum += nums[r];            
+            while(windowSum > (sum - x)){
+                windowSum -= nums[l]; // reducing window size
+                l++;
             }            
-            if(windowSum == s - x){
-                ans = min(ans, n - (end - st + 1));
+            if(windowSum == (sum - x)){
+                ans = min(ans, n - (r - l + 1));
             }            
-            end++;            
+            r++;            
         }
 		
         return (ans == 1000000 ? -1 : ans);
