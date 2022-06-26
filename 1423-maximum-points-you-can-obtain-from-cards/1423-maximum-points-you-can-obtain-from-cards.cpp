41035f28b64a66f50b828c1738d6acbf8dc1c30d
aenxso <32026487+aenxso@users.noreply.github.com>
2022-06-26 03:25:23 -0700

    Time: 129 ms (11.83%), Space: 42.4 MB (48.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
+        // sliding window problem
+        // make sure the number of elements outside the window is always k,
+        // because we are creating the sum from the k outside elements
+        // as the window moves to the right, add on the leftmost element and remove the rightmost element
+        //  as they shift with the bounds of the window
+        // Google's most asked question of 2021
+        
         int n = cardPoints.size(), l = 0, r = n - k, cur = 0, ans = 0;
-        for(int i = r; i < n; i++) {
+        for(int i = r; i < n; i++) { // get the current sum from the rightmost k elements
             cur += cardPoints[i];
         }
-        ans = cur;
+        ans = cur; // update answer
         
-        while(r < n) {
-            cur += (cardPoints[l] - cardPoints[r]);
+        while(r < n) { // while right window edge is in bounds
+            cur += (cardPoints[l] - cardPoints[r]); // add the leftmost element and remove the rightmost -- update cur
             ans = max(cur, ans);
-            l++;
+            l++; // increment window edges -- slide window
             r++;
         }
         return ans;
