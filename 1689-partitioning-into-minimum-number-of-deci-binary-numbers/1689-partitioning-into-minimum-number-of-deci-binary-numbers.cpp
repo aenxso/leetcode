4e1a6dcada74a7647a707f45516eaeb27bc8f7f1
aenxso <32026487+aenxso@users.noreply.github.com>
2022-06-27 04:31:46 -0700

    Time: 75 ms (10.66%), Space: 13.4 MB (92.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -6,7 +6,11 @@ public:
            eg. in 132, 3 is the biggest num
            eg. in 999, 9 is the biggest num
            eg. in 12, 2 is the biggest num 
-           - think of how many 'ones' are needed to make this max single digit*/
+           - think of how many 'ones' are needed to make this max single digit
+           - can also picture the number as having columns, and subtracting 1 from each
+             number until it reaches zero. This is why we just need to find the highest number,
+             because going down the column, that will be the last to reach 0. 
+             Taking x steps, x being the largest digit */
         
         char maxNum = '0';
         // sort(n.begin(), n.end());
