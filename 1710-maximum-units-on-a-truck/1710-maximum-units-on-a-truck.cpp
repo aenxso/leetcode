e8eea6392abc95fa0c19031509ec102b68435e06
aenxso <32026487+aenxso@users.noreply.github.com>
2022-07-01 01:32:25 -0700

    Time: 238 ms (24.87%), Space: 50.5 MB (5.95%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        // sort and use greedy approach 
+        
         int ans = 0;
-        sort(boxTypes.begin(), boxTypes.end(),[](vector<int> a, vector<int> b) {
+        sort(boxTypes.begin(), boxTypes.end(),[](vector<int> a, vector<int> b) { // custom sort for second value in decreasing order
            return a[1] > b[1]; 
         });
         
         for(auto box : boxTypes) {
-            int limit = min(box[0], truckSize); 
-            ans += limit * box[1];
-            truckSize -= limit;
-            if(!truckSize) break;
+            int limit = min(box[0], truckSize); // choose numberOfBoxes which is smaller than the truck size
+            ans += limit * box[1];              // increase ans by numOfBoxes * numOfUnits      
+            truckSize -= limit;                 // decrease truck size so we don't go over
+            if(!truckSize) break;               // capacity is full, exit loop
         }
         return ans;
     }
