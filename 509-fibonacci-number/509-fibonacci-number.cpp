0bc269b8162a1bf8860a12a72b768e8c32caa79f
aenxso <32026487+aenxso@users.noreply.github.com>
2022-07-06 05:13:17 -0700

    Time: 10 ms (38.76%), Space: 6 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    int memo[31] = {0};
     int fib(int n) {
-        if(n <= 1) {
+        // recursion
+        if(n <= 1) { // base condition
             return n;
         }
-        if(memo[n]) {
-            return memo[n];
-        }
-        return memo[n] = fib(n - 1) + fib(n - 2);
+        return fib(n - 1) + fib(n - 2);
     }
 };
