8ce0a92fdb196aa0f453e9f8a335676e24aba182
aenxso <32026487+aenxso@users.noreply.github.com>
2022-07-07 05:10:13 -0700

    Time: 14 ms (39.34%), Space: 6.5 MB (72.32%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -7,11 +7,15 @@ public:
         vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1, false));
         dp[0][0] = true;
         
-        for(int i2 = 1; i2 <= n2; i2++) dp[0][i2] = dp[0][i2 - 1] && s2[i2 - 1] == s3[i2 - 1];
-        for(int i1 = 1; i1 <= n1; i1++) dp[i1][0] = dp[i1 - 1][0] && s1[i1 - 1] == s3[i1 - 1];
+        for(int i2 = 1; i2 <= n2; i2++) {
+            dp[0][i2] = dp[0][i2 - 1] && s2[i2 - 1] == s3[i2 - 1];
+        }
+        for(int i1 = 1; i1 <= n1; i1++) {
+            dp[i1][0] = dp[i1 - 1][0] && s1[i1 - 1] == s3[i1 - 1];
+        }
 
-        for(int i1 = 1; i1 <= n1; i1++){
-            for(int i2 = 1; i2 <= n2; i2++){
+        for(int i1 = 1; i1 <= n1; i1++) {
+            for(int i2 = 1; i2 <= n2; i2++) {
                 dp[i1][i2] = (dp[i1 - 1][i2] && s1[i1 - 1] == s3[i1 + i2 - 1]) || (dp[i1][i2 - 1] && s2[i2 - 1] == s3[i1 + i2 - 1]);
             }
         }
