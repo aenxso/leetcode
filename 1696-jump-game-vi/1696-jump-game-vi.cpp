616816cf11153eb9441b9e4b24190fd97ca88949
aenxso <32026487+aenxso@users.noreply.github.com>
2022-07-09 04:47:06 -0700

    Time: 375 ms (36.03%), Space: 80.8 MB (70.83%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -7,8 +7,9 @@ public:
         for(int i = 1; i < size(nums); i++) {
             if(q.front() < i - k) q.pop_front();         // can't reach current index from index stored in q     
             dp[i] = nums[i] + dp[q.front()];             // update max score for current index
-            while(!q.empty() && dp[q.back()] <= dp[i])   // pop indices which won't be ever chosen in the future
+            while(!q.empty() && dp[q.back()] <= dp[i]) { // pop indices which won't be ever chosen in the future
                 q.pop_back();
+            }
             q.push_back(i);                              // insert current index
         }
         return dp.back();
