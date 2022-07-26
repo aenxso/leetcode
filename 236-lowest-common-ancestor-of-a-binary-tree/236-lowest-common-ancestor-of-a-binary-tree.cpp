4a754ae283f6612f58d0e8706a9dbc59b536c5d0
aenxso <32026487+aenxso@users.noreply.github.com>
2022-07-26 04:41:31 -0700

    Time: 17 ms (87.17%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,43 +10,16 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // naive approach
-        // store the path to both nodes in vector
-        // go through vector and find the common ancestor
-        
-        if(!root) return {};
-        TreeNode* ans;
-        
-        vector<TreeNode*> path1, path2;
-        
-        getPath(root, path1, p->val);
-        getPath(root, path2, q->val);
-        
-        int n = min(path1.size(), path2.size());
-        
-        for(int i = 0; i < n; i++) { // get the lowest by continuously going through the vector until we reach the end of the shorter path
-            if(path1[i] == path2[i]) {
-                ans = path1[i]; 
-            } else {
-                break;
-            }
+        if(!root || root == p || root == q) {
+            return root;
         }
         
-        return ans;
-    }
-    
-private:
-    bool getPath(TreeNode* root, vector<TreeNode*> &path, int x) {
-        if(!root) return false;
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
         
-        path.push_back(root); // add current node to path
-        
-        if(root->val == x) return true; // if x is found, return
-        
-        if(getPath(root->left, path, x) || getPath(root->right, path, x)) return true; // recurse through left and right subtree
-        
-        path.pop_back();
-        
-        return false;
+        if(l && r) {
+            return root;
+        }
+        return l ? l : r;
     }
 };
