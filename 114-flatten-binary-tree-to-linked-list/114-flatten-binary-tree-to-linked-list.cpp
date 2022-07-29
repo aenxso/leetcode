b6a8e666d25c44ae3d425b0a7b5b5cd658b36a81
aenxso <32026487+aenxso@users.noreply.github.com>
2022-07-29 05:16:35 -0700

    Time: 13 ms (33.77%), Space: 12.7 MB (38.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -14,18 +14,18 @@ public:
     void flatten(TreeNode* root) {
         if (!root) return;
         
-        flatten(root->left);
-        flatten(root->right);
+        flatten(root->left);  // go all the way to the left tree
+        flatten(root->right); // once reached the bottom of the left tree, go to the right children
         
-        TreeNode *tmp = root->right;
+        TreeNode *tmp = root->right; // create tmp node to store the right subtree
         
-        root->right = root->left;
-        root->left = NULL;
+        root->right = root->left; // make the root point to the left tree, now making it it's right subtree (moving the left over to the right)
+        root->left = NULL;        // clear the left subtree
         
-        while (root->right) {
-            root = root->right;
+        while (root->right) {   // move the pointer for root all the way down to the last newly placed right node
+            root = root->right; //  so when we re-attach the right subtree it's attached at the bottom of the 'list'
         }
         
-        root->right = tmp;
+        root->right = tmp; // attach the stored original right subtree
     }
 };
