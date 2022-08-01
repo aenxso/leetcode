df5e98b384dfec27cbb6af918bbb63f2b50edcb8
aenxso <32026487+aenxso@users.noreply.github.com>
2022-07-31 23:36:30 -0700

    Time: 184 ms (31.43%), Space: 14.5 MB (67.02%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // create array to store min number of coins needed to make i cents
-        // array length = amount + 1, because amount is the end goal. + 1 for accounting for 0
+        // dynamic programming - find the min number of coins needed to make i cents
         
-        sort(coins.begin(), coins.end()); // optimization - break out of for loop if the coin is too big - leetcode reports as slower tho
-        vector<int> dp(amount + 1, (amount + 1)); // fill array with amount + 1 because we need a higher amount to compare 
-                                                // with in min function, and the value at dp[i] will not exceed amount
-        dp[0] = 0;
-        for(int i = 1; i <= amount; i++) {
-            for(int j = 0; j < coins.size(); j++) {
-                if(coins[j] <= i) { // if the value of the coin is <= to the amount of cents we're trying to reach (i)
-                    dp[i] = min(dp[i], (dp[i - coins[j]] + 1)); // min of current value we have at ddp[i] or taking current
+        sort(coins.begin(), coins.end());
+        vector<int> dp (amount + 1, (amount + 1)); // size is amount+1 to account for 0 cents, filling dp with amount+1
+                                                   // because we need a higher amount to compare to when we reach the min() function
+        dp[0] = 0; // 0 cents
+        
+        for(int i = 1; i <= amount; i++) {          // first loop is to match i cents, up to the amount
+            for(int j = 0; j < coins.size(); j++) { // second loop is to go through the coins available
+                if(coins[j] <= i) {                 // if coins[j] is <= the amount we're currently trying to reach, which is the value of i
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1); // min of current value we have at ddp[i] or taking current
                                                               // coin (this is the + 1) and a value previously computed for 
                                                               // remaining value needed to reach amount
                 } else {
@@ -19,7 +19,7 @@ public:
                 }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount]; // if dp[amount] hasn't changhed from what we initally set it to, ie (amount+1)
-                                                      // then we know we never reached a solution for it
+        return dp[amount] > amount ? -1 : dp[amount]; // if dp[amount] is still great than amount (because it was set to amount+1)
+                                                      // then we know we never reached a solution
     }
 };
