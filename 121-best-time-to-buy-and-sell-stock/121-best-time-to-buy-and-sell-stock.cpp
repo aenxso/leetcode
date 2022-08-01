53dc8ccf352a0345ea9d344c12e3df1ccde0737f
aenxso <32026487+aenxso@users.noreply.github.com>
2022-07-31 23:49:26 -0700

    Time: 231 ms (34.02%), Space: 93.2 MB (99.14%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        // don't have to worry about selling in the past because that logic never gets coded
+        
         int ans = 0, lowest = INT_MAX;
         
         for(int i = 0; i < prices.size(); i++) {
-            lowest = min(prices[i], lowest);
-            int curr = prices[i] - lowest;
-            ans = max(ans, curr);
+            lowest = min(prices[i], lowest); // keep track of lowest value found so far
+            int curr = prices[i] - lowest;   // compute current profit that would be made if sold today
+            ans = max(ans, curr);            // keep track of max profit seen so far
         }
         return ans;
     }
