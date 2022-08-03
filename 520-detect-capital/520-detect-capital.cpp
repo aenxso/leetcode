a6c03fb0fbaaf8df268a2161a4a30dcf65e82bdd
aenxso <32026487+aenxso@users.noreply.github.com>
2022-08-03 04:20:00 -0700

    Time: 4 ms (34.48%), Space: 6.1 MB (65.43%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        if(isupper(word[0]) && isupper(word[1])) {
-            for(int i = 2; i < word.length(); i++) { // checking all uppercase
+        if(isupper(word[0]) && isupper(word[1])) { // check for word where ALL letters are capital
+            for(int i = 2; i < word.length(); i++) {
                 if(islower(word[i])) return false;
             }
         } else {
-            for(int i = 1; i < word.length(); i++) {
-                if(isupper(word[i])) return false;  // checking all lower OR only first letter is capital
+            for(int i = 1; i < word.length(); i++) { // checks for word where either first is upper or not, and all others are lower
+                if(isupper(word[i])) return false;   // we don't have to explicitly check what the first letter is because it's either upper
+                                                     //  or lower, and both are okay in the scenarios we are checking
             }
         }
         return true;
