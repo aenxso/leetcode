4edcb7f285c6a2d655573e6ce03931d32b190c2c
aenxso <32026487+aenxso@users.noreply.github.com>
2022-08-07 01:40:00 -0700

    Time: 51 ms (43.31%), Space: 25 MB (54.69%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // mod k by nums.size() in case k is bigger than amount of elements in nums
-        // reverse 0->k, reverse k->end, reverse 0->end
+        // reverse nums[0] to nums[n-k] of the array, and nums[n-k+1] to nums[n-1], then reverse the whole array again
+        // modulo k with nums.size in case k is some huge number and would go out of bounds with the above operation
         
         int n = nums.size();
-        int mod = k % n;
+        int newK = k % n;
         
-        reverse(nums.begin(), nums.end() - mod);
-        reverse(nums.end() - mod, nums.end());
+        reverse(nums.begin(), nums.end() - newK);
+        reverse(nums.end() - newK, nums.end());
         reverse(nums.begin(), nums.end());
     }
 };
