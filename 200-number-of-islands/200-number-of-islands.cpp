15a374a85136c50a6eb8a1479b3b738a40c8eb7c
aenxso <32026487+aenxso@users.noreply.github.com>
2022-08-07 01:28:11 -0700

    Time: 64 ms (37.79%), Space: 12.2 MB (83.09%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -10,12 +10,13 @@ public:
                 }
             }
         }
+        
         return ans;
     }
     
 private:
     int dfs(vector<vector<char>>& grid, int i, int j) {
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0') {
             return 0;
         }
         
@@ -24,6 +25,7 @@ private:
         dfs(grid, i - 1, j);
         dfs(grid, i, j + 1);
         dfs(grid, i, j - 1);
+        
         return 1;
     }
 };
