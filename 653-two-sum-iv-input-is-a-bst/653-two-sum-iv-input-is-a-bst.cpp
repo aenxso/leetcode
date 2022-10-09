8318c7220c461331e717211f3cb414c6aebdc860
aenxso <32026487+aenxso@users.noreply.github.com>
2022-10-09 05:02:47 -0700

    Time: 81 ms (45.38%), Space: 36.9 MB (62.65%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     void inorder(TreeNode* root) {
-        if (!root) return;
+        if(!root) return;
         inorder(root->left);
         vec.push_back(root->val);
         inorder(root->right);
@@ -21,11 +21,15 @@ public:
     bool findTarget(TreeNode* root, int k) {
         inorder(root);
         int l = 0, r = vec.size()-1;
-        while (l < r) {
-            if (vec[l] + vec[r] == k) return true;
-            else {
-                if (vec[l] + vec[r] < k) l++;
-                else r--;
+        while(l < r) {
+            if(vec[l] + vec[r] == k) {
+                return true;
+            } else {
+                if(vec[l] + vec[r] < k) {
+                    l++;
+                } else {
+                    r--;
+                }
             }
         }
         return false;
