df12362ae4589265f692e786407dc3a0f0f4a3c2
aenxso <32026487+aenxso@users.noreply.github.com>
2022-10-14 05:41:33 -0700

    Time: 1025 ms (98.74%), Space: 294.9 MB (20.46%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,17 +11,15 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        // edge case conditions
-       if(head == NULL) {
-          return NULL;
-       }
+       if(head == NULL) return NULL;
+       
        if(head->next == NULL) {
           delete head;
           return NULL;
        }
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* prev = head; // keeping this prev node to get the previous node of slow pointer when we reach middle
+        
+        ListNode *slow = head, *fast = head, *prev = head; // keeping this prev node to get the 
+                                                           // previous node of slow pointer when we reach middle
         while(fast != NULL && fast->next != NULL) {
             if(slow != head) prev = prev->next; // since we want prev to start one step later than slow node
             slow = slow->next;
