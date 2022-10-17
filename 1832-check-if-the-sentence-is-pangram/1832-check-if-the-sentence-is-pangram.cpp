96c595fcee9609722d5dc686ad1277f4e0def294
aenxso <32026487+aenxso@users.noreply.github.com>
2022-10-17 04:29:17 -0700

    Time: 13 ms (7.09%), Space: 7.1 MB (6.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
+        // create an unordered set that contains every character in the given sentence
+        // if every letter is present, the set size will be 26
+        
         unordered_set<char> chars (sentence.begin(), sentence.end());
         return chars.size() == 26;
     }
