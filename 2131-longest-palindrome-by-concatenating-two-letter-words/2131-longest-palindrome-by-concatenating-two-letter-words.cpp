c15e6092d92e92f16d65b578181524594585f498
aenxso <32026487+aenxso@users.noreply.github.com>
2022-11-03 05:31:39 -0700

    Time: 1748 ms (5.05%), Space: 168 MB (33.22%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        int count = 0;
+        // Create map and count all words frequency
+        map<string, int> m;
+        for(auto w : words) {
+            m[w]++;
+        }
+        
+        // if single word like gg once done then not consider second time
+        // ex. cd*gg*dc
+        bool flag = false;
+        for(auto x : words) {
+            string w = x;
+            reverse(w.begin(),w.end());
+            // encounter bc and cb then both freq -- by 1
+            if(w != x && m[x] > 0 && m[w] > 0) {
+                m[x]--;
+                m[w]--;
+                count += 4;
+            } else if(w == x && m[x] > 1) { // encounter aa and aa 2 times like bc*aa aa*cb,-- by 2 times
+                m[x] -= 2;
+                count += 4;
+            } else if(w == x && !flag && m[x] > 0) { // consider once like, bc*gg*cb
+                m[x]--;
+                count += 2;
+                flag = true;
+            }
+        }
+        return count;
+    }
+};
