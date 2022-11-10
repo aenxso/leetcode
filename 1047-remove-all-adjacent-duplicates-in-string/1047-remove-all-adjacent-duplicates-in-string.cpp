822c3782df0411d1589e73710798d024cbb5347e
aenxso <32026487+aenxso@users.noreply.github.com>
2022-11-10 02:33:16 -0800

    Time: 40 ms (67.76%), Space: 10.9 MB (66.38%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -11,7 +11,7 @@ public:
             if(ans.size() && ans.back() == s[i]) { // if ans has letters in it and last letter matches s[i]
                 ans.pop_back();
             } else { 
-                ans.push_back(s[i]);
+                ans.push_back(s[i]); // add letter onto ans if not duplicate
             }
         }
         
